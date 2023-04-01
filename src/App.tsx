678c693e1b53962b3c4e670cf4282477449e9ffe
Vilija Mažeikė <vmazeike@reiz.tech>
2023-04-01 18:59:30 +0300

    add an image from new assets file, declare a module, update config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./styles.css";
+import WEBPACKLOGO from "./assets/webpack-logo.png";
 
 export const App = () => {
-  return <h1>React Typescript Webpack project for IDP</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack project for IDP</h1>
+      <img src={WEBPACKLOGO} alt="Webpack logo" width="200" height="200" />
+    </>
+  );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
         use: [{ loader: "babel-loader" }],
       },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(?:png|jpg|jpeg)$/, type: "asset/resource" },
     ],
   },
   output: {
