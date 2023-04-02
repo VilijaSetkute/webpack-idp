98991352e74cd3e5a9a1506a81e6f21db38e7276
Vilija Mažeikė <vmazeike@reiz.tech>
2023-04-02 13:50:47 +0300

    change ap components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import "./styles.css";
 import WEBPACKLOGO from "./assets/webpack-logo.png";
 import WEATHER from "./assets/Weather_icon_-_sunny.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
   return (
     <>
-      <h1>React Typescript Webpack project for IDP</h1>
+      <h1>
+        Edited, React Typescript Webpack project for IDP -{" "}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={WEBPACKLOGO} alt="Webpack logo" width="200" height="200" />
       <img src={WEATHER} alt="Webpack logo" width="200" height="200" />
+      <ClickCounter />
     </>
   );
 };
