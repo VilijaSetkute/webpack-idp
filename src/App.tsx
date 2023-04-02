67d777ec924eae786423a26667ea8768fab970d4
Vilija Mažeikė <vmazeike@reiz.tech>
2023-04-02 14:34:13 +0300

    test prettier and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import "./styles.css";
-import WEBPACKLOGO from "./assets/webpack-logo.png";
-import WEATHER from "./assets/Weather_icon_-_sunny.svg";
-import { ClickCounter } from "./ClickCounter";
+import './styles.css';
+import WEBPACKLOGO from './assets/webpack-logo.png';
+import WEATHER from './assets/Weather_icon_-_sunny.svg';
+import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
   return (
     <>
       <h1>
-        Edited, React Typescript Webpack project for IDP -{" "}
+        Edited, React Typescript Webpack project for IDP -{' '}
         {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={WEBPACKLOGO} alt="Webpack logo" width="200" height="200" />
