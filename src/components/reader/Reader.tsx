bb23a38f9dbe19562967102e59bc8a83b228ef63
Vilija Mažeikė <vmazeike@reiz.tech>
2023-04-23 16:48:34 +0300

    wrap output and form into single component

---
+++ src/components/reader/Reader.tsx
@@ -0,0 +1,36 @@
+import './styles.css';
+import { Form } from '../form/Form';
+import { Output } from '../output/Output';
+import { useEffect, useState } from 'react';
+import { defaultText, defaultOptions } from '../../constants/defaults';
+import { DefaultOptions } from '../../utils/models/model';
+
+export const Reader = () => {
+  const [text, setText] = useState<string>(defaultText);
+  const [selectedOptions, setSelectedOptions] =
+    useState<DefaultOptions>(defaultOptions);
+  const [isContrastEnabled, setIsContrastEnabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (selectedOptions.fixation !== 'none') {
+      setIsContrastEnabled(true);
+    } else {
+      setIsContrastEnabled(false);
+    }
+  }, [selectedOptions.fixation]);
+
+  const stringToArray = (text: string) => {
+    return text.split(' ');
+  };
+
+  return (
+    <div className="reader-container">
+      <Form
+        setText={setText}
+        setSelectedOptions={setSelectedOptions}
+        isContrastEnabled={isContrastEnabled}
+      />
+      <Output selectedOptions={selectedOptions} text={stringToArray(text)} />
+    </div>
+  );
+};
