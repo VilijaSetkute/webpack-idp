c77a2e48cecf69f7d5c515aea72327f60a0d0c1d
Vilija Mažeikė <vmazeike@reiz.tech>
2023-04-23 16:48:56 +0300

    use deafult values

---
+++ src/constants/defaults.ts
@@ -0,0 +1,11 @@
+export const exampleWord = 'Bionic';
+export const defaultText =
+  'Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic reading aims to encourage a more in-depth reading and understanding of written content.';
+
+export const MIN_FONT_SIZE = 12;
+export const MAX_FONT_SIZE = 80;
+
+export const defaultOptions = {
+  fixation: 'none',
+  contrast: 'standard',
+};
