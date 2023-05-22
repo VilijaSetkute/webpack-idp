35d2b9b1977051e8aecac7aa9fc827bdd4d23475
Vilija Mažeikė <vmazeike@reiz.tech>
2023-05-22 09:00:29 +0300

    use button component

---
+++ src/utils/UI/button/Button.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import './styles.scss';
+
+type Variant = 'primary' | 'secondary';
+type ButtonWidth = 'content' | 'component';
+
+interface ButtonProps {
+  text?: string;
+  icon?: JSX.Element;
+  variant: Variant;
+  buttonWidth: ButtonWidth;
+  capitalize?: boolean;
+  weight?: boolean;
+  onClick: () => void;
+}
+
+export const Button: FC<ButtonProps> = ({
+  text,
+  icon,
+  variant,
+  buttonWidth,
+  capitalize = false,
+  weight = false,
+  onClick,
+}) => {
+  return (
+    <button
+      className={`custom-button ${variant} ${buttonWidth}`}
+      style={{
+        fontWeight: weight ? 'bold' : 'initial',
+      }}
+      onClick={onClick}
+    >
+      {icon && icon}
+      {capitalize && text ? text.toUpperCase() : text}
+    </button>
+  );
+};
