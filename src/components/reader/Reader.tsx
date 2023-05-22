077ff2c7929b7c6f55283103f4716b53a655bdd4
Vilija Mažeikė <vmazeike@reiz.tech>
2023-05-22 09:03:17 +0300

    update reader

--- src/components/reader/Reader.tsx
+++ src/components/reader/Reader.tsx
@@ -1,37 +1,45 @@
 import './styles.scss';
-import { Form } from '../form/Form';
-import { Output } from '../output/Output';
-import { useEffect, useState } from 'react';
-import { defaultText, defaultOptions } from '../../constants/defaults';
-import { DefaultOptions } from '../../utils/models/model';
+import { useContext, useState } from 'react';
+import { Dialog } from '../../utils/UI/dialog';
+import { DataContext } from '../../utils/context/dataContext';
+import { ReaderCard } from '../readerCard/ReaderCard';
+import { Button } from '../../utils/UI/button';
+import { Form } from '../form';
 
 export const Reader = () => {
-  const [text, setText] = useState<string>(defaultText);
-  const [selectedOptions, setSelectedOptions] =
-    useState<DefaultOptions>(defaultOptions);
-  const [isContrastEnabled, setIsContrastEnabled] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (selectedOptions.fixation !== 'none') {
-      setIsContrastEnabled(true);
-    } else {
-      setIsContrastEnabled(false);
-    }
-  }, [selectedOptions.fixation]);
-
-  const stringToArray = (text: string) => {
-    return text.split(' ');
-  };
+  const [showFormModal, setShowFormModal] = useState<boolean>(false);
+  const { bionicList } = useContext(DataContext);
 
   return (
     <div className="reader-container">
-      <Form
-        setText={setText}
-        setSelectedOptions={setSelectedOptions}
-        isContrastEnabled={isContrastEnabled}
-        selectedOptions={selectedOptions}
-      />
-      <Output selectedOptions={selectedOptions} text={stringToArray(text)} />
+      <div className="reader-intro">
+        <h3>What is bionic reading</h3>
+        <div>
+          In a digital world dominated by shallow forms of reading, Bionic
+          reading aims to encourage a more in-depth reading and understanding of
+          written content by facilitating the reading process by guiding the
+          eyes through text with artificial fixation points.
+        </div>
+        <Button
+          text="Try me out"
+          variant="primary"
+          buttonWidth="component"
+          capitalize={true}
+          weight={true}
+          onClick={() => setShowFormModal(true)}
+        />
+        <div>
+          <h3 className="reader--subtitle">Saved bionic reading</h3>
+          {bionicList.map((el) => (
+            <ReaderCard key={el.id} listItem={el} />
+          ))}
+        </div>
+        {showFormModal && (
+          <Dialog onClose={() => setShowFormModal(false)}>
+            <Form />
+          </Dialog>
+        )}
+      </div>
     </div>
   );
 };
