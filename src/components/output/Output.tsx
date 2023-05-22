db35b5a20c32ddb7485ace7d2d185aa7262e7f76
Vilija Mažeikė <vmazeike@reiz.tech>
2023-05-22 09:03:03 +0300

    update output

--- src/components/output/Output.tsx
+++ src/components/output/Output.tsx
@@ -1,67 +1,23 @@
-import { FC, useState } from 'react';
-import { MIN_FONT_SIZE, MAX_FONT_SIZE } from '../../constants/defaults';
-import INCREASE from '../../assets/FontIncrease.svg';
-import DECREASE from '../..//assets/FontDecrease.svg';
-import RESET from '../..//assets/ResetIcon.svg';
+import { FC } from 'react';
 import './styles.scss';
 import { applyReader } from '../../utils/helpers/applyReader';
-import { DefaultOptions } from '../../utils/models/model';
+import { BionicItem } from '../../utils/models/model';
 
 interface Output {
-  selectedOptions: DefaultOptions;
-  text: string[];
+  selectedOptions: BionicItem;
 }
 
-export const Output: FC<Output> = ({ selectedOptions, text }) => {
-  const [textFontSize, setTextFontSize] = useState(16);
-
-  const increaseFontSize = () => {
-    if (textFontSize < MAX_FONT_SIZE) {
-      setTextFontSize(textFontSize + 2);
-    }
-  };
-
-  const decreaseFontSize = () => {
-    if (textFontSize > MIN_FONT_SIZE) {
-      setTextFontSize(textFontSize - 2);
-    }
-  };
-
+export const Output: FC<Output> = ({ selectedOptions }) => {
   return (
-    <div>
-      <div className="output__title">Text options</div>
-      <div className="flex-horizontal-end">
-        <div>
-          <img
-            className="output_resizer output_resizer--left"
-            src={INCREASE}
-            alt="Font increase"
-            height={20}
-            onClick={increaseFontSize}
-          />
-          <img
-            className="output_resizer"
-            src={DECREASE}
-            alt="Font decrease"
-            height={20}
-            onClick={decreaseFontSize}
-          />
-          <img
-            className="output_resizer output_resizer--right"
-            src={RESET}
-            alt="Reset font change"
-            height={20}
-            onClick={() => setTextFontSize(16)}
-          />
-        </div>
-      </div>
-      <div style={{ fontSize: textFontSize }}>
-        {text.map((word, idx) => (
-          <span key={idx} className="inline-block">
-            {applyReader(selectedOptions, word)}
-          </span>
-        ))}
-      </div>
+    <div
+      className="output__spacing"
+      style={{ fontSize: selectedOptions.fontSize }}
+    >
+      {selectedOptions.text.map((word, idx) => (
+        <span key={idx} className="inline-block">
+          {applyReader(selectedOptions, word)}
+        </span>
+      ))}
     </div>
   );
 };
