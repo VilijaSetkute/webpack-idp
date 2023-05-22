d53bd3174449b618eaf9d3f425137e650e6465e6
Vilija Mažeikė <vmazeike@reiz.tech>
2023-05-22 09:02:48 +0300

    update form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,129 +1,139 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 import './styles.scss';
 import { OptionCard } from '../optionCard';
-import { FC, useRef } from 'react';
-import { defaultOptions, defaultText } from '../../constants/defaults';
-import { FormProps } from '../../utils/models/model';
+import { FC, useRef, useState } from 'react';
+import { defaultOptions } from '../../constants/defaults';
+import { DefaultOptions } from '../../utils/models/model';
+import { optionSelections } from '../../constants/defaults';
+import {
+  MIN_FONT_SIZE,
+  MAX_FONT_SIZE,
+  FONT_STEP,
+} from '../../constants/defaults';
+import { Output } from '../output';
+import { Button } from '../../utils/UI/button';
+import { useCreateForm } from '../../utils/hooks/useCreateForm';
 
-export const Form: FC<FormProps> = ({
-  setText,
-  setSelectedOptions,
-  isContrastEnabled,
-  selectedOptions,
-}) => {
+export const Form: FC = () => {
+  const [text, setText] = useState<string>('');
+  const [textFontSize, setTextFontSize] = useState(16);
+  const [selectedOptions, setSelectedOptions] =
+    useState<DefaultOptions>(defaultOptions);
   let customTextRef = useRef<HTMLTextAreaElement>(null);
+  const { submit, methods } = useCreateForm();
+
+  const stringToArray = (text: string) => {
+    return text.split(' ');
+  };
+
   const updateText = () => {
     const useText = customTextRef.current?.value;
     useText && setText(useText);
+
+    useText && methods.setValue('text', stringToArray(useText));
   };
-  const resetDefaultText = () => {
-    setText(defaultText);
-    if (customTextRef.current) {
-      customTextRef.current.value = '';
-    }
-  };
-  const updateOptions = (key: any, value: any) => {
+
+  const updateOptions = (key: 'fixation' | 'contrast', value: string) => {
     if (key === 'fixation' && value === 'none') {
       setSelectedOptions(defaultOptions);
     } else {
       setSelectedOptions((prev) => ({ ...prev, [key]: value }));
     }
+    methods.setValue(`${key}`, value);
+  };
+
+  const increaseFontSize = () => {
+    if (textFontSize < MAX_FONT_SIZE) {
+      setTextFontSize(textFontSize + FONT_STEP);
+    }
+    methods.setValue('fontSize', textFontSize + FONT_STEP);
+  };
+
+  const decreaseFontSize = () => {
+    if (textFontSize > MIN_FONT_SIZE) {
+      setTextFontSize(textFontSize - FONT_STEP);
+    }
+    methods.setValue('fontSize', textFontSize - FONT_STEP);
   };
 
   return (
     <div className="form">
-      <div>
-        <div>
-          <textarea
-            className="form__input--text"
-            ref={customTextRef}
-            placeholder="Enter your custom text"
-          />
-
-          <div className="form__input--buttons">
-            <button className="mr-8" onClick={updateText}>
-              Update
-            </button>
-            <button onClick={resetDefaultText}>Reset</button>
-          </div>
-        </div>
-      </div>
-      <div className="form__options--title">Bionic reader options</div>
+      <textarea
+        className="form__input--text"
+        ref={customTextRef}
+        placeholder="Enter your custom text"
+        onChange={updateText}
+      />
       <div className="form__options">
-        <div className="mr-48">
-          <p className="form__option--subtitle">Fixation: </p>
+        <div>
+          <p className="form__options--subtitle">Fixation: </p>
           <div className="center-vertical">
-            <div onClick={() => updateOptions('fixation', 'none')}>
-              <OptionCard
-                option="fixation"
-                optionTitle="none"
-                fixation="none"
-                isSelected={selectedOptions.fixation === 'none'}
-              />
-            </div>
-
-            <div onClick={() => updateOptions('fixation', 'low')}>
-              <OptionCard
-                option="fixation"
-                optionTitle="low"
-                fixation="low"
-                isSelected={selectedOptions.fixation === 'low'}
-              />
-            </div>
-
-            <div onClick={() => updateOptions('fixation', 'standard')}>
-              <OptionCard
-                option="fixation"
-                optionTitle="standard"
-                fixation="standard"
-                isSelected={selectedOptions.fixation === 'standard'}
-              />
-            </div>
-
-            <div onClick={() => updateOptions('fixation', 'strong')}>
-              <OptionCard
-                option="fixation"
-                optionTitle="strong"
-                fixation="strong"
-                isSelected={selectedOptions.fixation === 'strong'}
-              />
-            </div>
+            {optionSelections['fixation'].map((el) => (
+              <div onClick={() => updateOptions('fixation', el)}>
+                <OptionCard
+                  option="fixation"
+                  optionTitle={el}
+                  fixation={el}
+                  isSelected={selectedOptions.fixation === el}
+                />
+              </div>
+            ))}
           </div>
         </div>
 
-        {isContrastEnabled && (
-          <div>
-            <p className="form__options--subtitle">Contrast: </p>
-            <div className="center-vertical">
-              <div onClick={() => updateOptions('contrast', 'standard')}>
+        <div>
+          <p className="form__options--subtitle">Contrast: </p>
+          <div className="center-vertical">
+            {optionSelections['contrast'].map((el) => (
+              <div onClick={() => updateOptions('contrast', el)}>
                 <OptionCard
                   option="contrast"
-                  optionTitle="standard"
-                  contrast="standard"
-                  isSelected={selectedOptions.contrast === 'standard'}
+                  optionTitle={el}
+                  contrast={el}
+                  isSelected={selectedOptions.contrast === el}
                 />
               </div>
-              <div onClick={() => updateOptions('contrast', 'high')}>
-                <OptionCard
-                  option="contrast"
-                  optionTitle="high"
-                  contrast="high"
-                  isSelected={selectedOptions.contrast === 'high'}
-                />
-              </div>
-              <div onClick={() => updateOptions('contrast', 'low')}>
-                <OptionCard
-                  option="contrast"
-                  optionTitle="low"
-                  contrast="low"
-                  isSelected={selectedOptions.contrast === 'low'}
-                />
-              </div>
-            </div>
+            ))}
           </div>
-        )}
+        </div>
+
+        <div>
+          <p className="form__options--subtitle">Font size: </p>
+          <div className="center-vertical">
+            <Button
+              icon={<FontAwesomeIcon icon={faMinus} />}
+              variant="primary"
+              buttonWidth="content"
+              onClick={decreaseFontSize}
+            />
+            <div className="center-vertical form__button--font-size">
+              {textFontSize}
+            </div>
+            <Button
+              icon={<FontAwesomeIcon icon={faPlus} />}
+              variant="primary"
+              buttonWidth="content"
+              onClick={increaseFontSize}
+            />
+          </div>
+        </div>
       </div>
+
+      <div className="from__preview--separator">Preview</div>
+      {!!text ? (
+        <Output selectedOptions={selectedOptions} />
+      ) : (
+        <div className="from__preview--no-text">No text provided</div>
+      )}
+
+      <Button
+        text="save"
+        capitalize={true}
+        variant="primary"
+        buttonWidth="component"
+        onClick={() => submit}
+      />
     </div>
   );
 };
