1bc2b6ae871ed18da03e509d3bd91dfc968d7c3b
Vilija Mažeikė <vmazeike@reiz.tech>
2023-05-22 08:59:17 +0300

    use context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './styles.scss';
+import { useState } from 'react';
 import { Header } from './components/header/Header';
 import { Footer } from './components/footer/Footer';
 import { Reader } from './components/reader/Reader';
+import { DataContext } from './utils/context/dataContext';
+import { initialCard } from './constants/defaults';
+import { BionicItem } from './utils/models/model';
 
 export const App = () => {
+  const [bionicList, setBionicList] = useState<BionicItem[]>([initialCard]);
+
   return (
-    <div className="App">
-      <Header />
-      <Reader />
-      <Footer />
-    </div>
+    <DataContext.Provider value={{ bionicList, setBionicList }}>
+      <div className="App">
+        <Header />
+        <Reader />
+        <Footer />
+      </div>
+    </DataContext.Provider>
   );
 };
