18af8b44a28321faadde8d96e0a58ee6bdb9a553
Vilija Mažeikė <vmazeike@reiz.tech>
2023-06-03 12:19:59 +0300

    use useMemo to memorize filtered results

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './styles.scss';
 import { useState } from 'react';
 import { Header } from './components/structure/header/Header';
@@ -6,13 +6,30 @@ import { Footer } from './components/structure/footer/Footer';
 import { Reader } from './components/structure/reader/Reader';
 import { DataContext } from './utils/context/dataContext';
 import { initialList } from '../src/utils/constants/defaults';
-import { BionicItem } from './utils/models/model';
+import { BionicItem, FilterOptions } from './utils/models/model';
 
 export const App = () => {
+  const [filter, setFilter] = useState<FilterOptions>({
+    fixation: 'fixation',
+    contrast: 'contrast',
+  });
   const [bionicList, setBionicList] = useState<BionicItem[]>(initialList);
 
+  const filteredList = useMemo(() => {
+    let filtered = bionicList;
+    if (filter.fixation !== 'fixation') {
+      filtered = filtered.filter((el) => el.fixation === filter.fixation);
+    }
+    if (filter.contrast !== 'contrast') {
+      filtered = filtered.filter((el) => el.contrast === filter.contrast);
+    }
+    return filtered;
+  }, [filter, bionicList]);
+
   return (
-    <DataContext.Provider value={{ bionicList, setBionicList }}>
+    <DataContext.Provider
+      value={{ filteredList, bionicList, setBionicList, setFilter }}
+    >
       <div className="App">
         <Header />
         <Reader />
