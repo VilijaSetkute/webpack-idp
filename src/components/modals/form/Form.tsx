c719a043fb1e7f0d0748e63c0d7ed630c03a2ec1
Vilija Mažeikė <vmazeike@reiz.tech>
2023-06-03 15:56:12 +0300

    aaply from wrapper styles instead of dialog style

--- src/components/modals/form/Form.tsx
+++ src/components/modals/form/Form.tsx
@@ -50,109 +50,111 @@ export const Form: FC<FormProps> = ({ id, onClose }) => {
   };
 
   return (
-    <div className="form">
-      <Controller
-        name="text"
-        control={methods.control}
-        rules={{ required: true }}
-        render={({ field }) => (
-          <>
-            <textarea
-              {...field}
-              className="form__input--text"
-              placeholder="Enter your custom text"
-              value={field.value}
-              onChange={(e) => methods.setValue(field.name, e.target.value)}
-            />
-            {errors.text && (
-              <div className="form__error">{errors.text.message}</div>
-            )}
-          </>
-        )}
-      />
-      <div className="form__options">
-        <div>
-          <p className="form__options--subtitle">Fixation: </p>
-          <div className="center-vertical">
-            {optionSelections['fixation'].map((el, idx) => (
-              <div key={idx} onClick={() => updateOptions('fixation', el)}>
-                <OptionCard
-                  option="fixation"
-                  optionTitle={el}
-                  fixation={el}
-                  isSelected={formFixation === el}
-                />
-              </div>
-            ))}
-          </div>
-          {errors.fixation && (
-            <div className="form__error mt-16">{errors.fixation.message}</div>
+    <div className="modal-container">
+      <div className="form">
+        <Controller
+          name="text"
+          control={methods.control}
+          rules={{ required: true }}
+          render={({ field }) => (
+            <>
+              <textarea
+                {...field}
+                className="form__input--text"
+                placeholder="Enter your custom text"
+                value={field.value}
+                onChange={(e) => methods.setValue(field.name, e.target.value)}
+              />
+              {errors.text && (
+                <div className="form__error">{errors.text.message}</div>
+              )}
+            </>
           )}
-        </div>
-
-        <div>
-          <p className="form__options--subtitle">Contrast: </p>
-          <div className="center-vertical">
-            {optionSelections['contrast'].map((el, idx) => (
-              <div key={idx} onClick={() => updateOptions('contrast', el)}>
-                <OptionCard
-                  option="contrast"
-                  optionTitle={el}
-                  contrast={el}
-                  isSelected={formContrast === el}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <div>
-          <p className="form__options--subtitle">Font size: </p>
-          <div className="center-vertical direction-row">
-            <Button
-              icon={<FontAwesomeIcon icon={faMinus} />}
-              variant="primary"
-              buttonWidth="content"
-              onClick={() => updateFontSize('decrease')}
-            />
-            <div className="center-vertical form__button--font-size">
-              {formFontSize}
+        />
+        <div className="form__options">
+          <div>
+            <p className="form__options--subtitle">Fixation: </p>
+            <div className="center-vertical">
+              {optionSelections['fixation'].map((el, idx) => (
+                <div key={idx} onClick={() => updateOptions('fixation', el)}>
+                  <OptionCard
+                    option="fixation"
+                    optionTitle={el}
+                    fixation={el}
+                    isSelected={formFixation === el}
+                  />
+                </div>
+              ))}
             </div>
-            <Button
-              icon={<FontAwesomeIcon icon={faPlus} />}
-              variant="primary"
-              buttonWidth="content"
-              onClick={() => updateFontSize('increase')}
-            />
+            {errors.fixation && (
+              <div className="form__error mt-16">{errors.fixation.message}</div>
+            )}
+          </div>
+
+          <div>
+            <p className="form__options--subtitle">Contrast: </p>
+            <div className="center-vertical">
+              {optionSelections['contrast'].map((el, idx) => (
+                <div key={idx} onClick={() => updateOptions('contrast', el)}>
+                  <OptionCard
+                    option="contrast"
+                    optionTitle={el}
+                    contrast={el}
+                    isSelected={formContrast === el}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <p className="form__options--subtitle">Font size: </p>
+            <div className="center-vertical direction-row">
+              <Button
+                icon={<FontAwesomeIcon icon={faMinus} />}
+                variant="primary"
+                buttonWidth="content"
+                onClick={() => updateFontSize('decrease')}
+              />
+              <div className="center-vertical form__button--font-size">
+                {formFontSize}
+              </div>
+              <Button
+                icon={<FontAwesomeIcon icon={faPlus} />}
+                variant="primary"
+                buttonWidth="content"
+                onClick={() => updateFontSize('increase')}
+              />
+            </div>
           </div>
         </div>
+
+        <div className="from__preview--separator">Preview</div>
+        {!!formtext.length ? (
+          <div style={{ padding: '0 16px' }}>
+            <Output
+              selectedOptions={{
+                fixation: formFixation,
+                contrast: formContrast,
+                fontSize: formFontSize,
+                text: formtext,
+              }}
+              textLength="truncated"
+              maxChars={150}
+            />
+          </div>
+        ) : (
+          <div className="from__preview--no-text">No text provided</div>
+        )}
+
+        <Button
+          text="save"
+          capitalize={true}
+          variant="primary"
+          buttonWidth="component"
+          onClick={methods.handleSubmit(submit)}
+        />
       </div>
-
-      <div className="from__preview--separator">Preview</div>
-      {!!formtext.length ? (
-        <div style={{ padding: '0 16px' }}>
-          <Output
-            selectedOptions={{
-              fixation: formFixation,
-              contrast: formContrast,
-              fontSize: formFontSize,
-              text: formtext,
-            }}
-            textLength="truncated"
-            maxChars={150}
-          />
-        </div>
-      ) : (
-        <div className="from__preview--no-text">No text provided</div>
-      )}
-
-      <Button
-        text="save"
-        capitalize={true}
-        variant="primary"
-        buttonWidth="component"
-        onClick={methods.handleSubmit(submit)}
-      />
     </div>
   );
 };
--- src/components/modals/reviewForm/ReviewForm.tsx
+++ src/components/modals/reviewForm/ReviewForm.tsx
@@ -19,46 +19,48 @@ export const ReviewForm: FC<ReviewFormProps> = ({ id, onEdit, onClose }) => {
   const editItem = filteredList.filter((bionic) => bionic.id === id);
 
   return (
-    <div className="review">
-      <FontAwesomeIcon
-        icon={faXmark}
-        size="xl"
-        className="icon icon-close"
-        onClick={() => onClose(false)}
-      />
-      <div className="review-header">
-        <div className="selection-spacing">
-          {!!editItem[0].fixation && (
-            <Chip text={`Fixation ${editItem[0].fixation}`} />
-          )}
-          {!!editItem[0].contrast && (
-            <Chip text={`Contrast ${editItem[0].contrast}`} />
-          )}
-          {editItem[0].fontSize !== 14 && (
-            <Chip text={`Font size ${editItem[0].fontSize}`} />
-          )}
+    <div className="modal-container">
+      <div className="review">
+        <FontAwesomeIcon
+          icon={faXmark}
+          size="xl"
+          className="icon icon-close"
+          onClick={() => onClose(false)}
+        />
+        <div className="review-header">
+          <div className="selection-spacing">
+            {!!editItem[0].fixation && (
+              <Chip text={`Fixation ${editItem[0].fixation}`} />
+            )}
+            {!!editItem[0].contrast && (
+              <Chip text={`Contrast ${editItem[0].contrast}`} />
+            )}
+            {editItem[0].fontSize !== 14 && (
+              <Chip text={`Font size ${editItem[0].fontSize}`} />
+            )}
+          </div>
+          <div>
+            <Button
+              text="Edit"
+              icon={<FontAwesomeIcon icon={faPen} />}
+              capitalize={false}
+              variant="secondary"
+              buttonWidth="component"
+              onClick={() => onEdit(id as string)}
+            />
+          </div>
         </div>
-        <div>
-          <Button
-            text="Edit"
-            icon={<FontAwesomeIcon icon={faPen} />}
-            capitalize={false}
-            variant="secondary"
-            buttonWidth="component"
-            onClick={() => onEdit(id as string)}
-          />
-        </div>
+        <div className="reader-card--date">{editItem[0].date}</div>
+        <Output
+          selectedOptions={{
+            fixation: editItem[0].fixation,
+            contrast: editItem[0].contrast,
+            fontSize: editItem[0].fontSize,
+            text: editItem[0].text,
+          }}
+          textLength="full"
+        />
       </div>
-      <div className="reader-card--date">{editItem[0].date}</div>
-      <Output
-        selectedOptions={{
-          fixation: editItem[0].fixation,
-          contrast: editItem[0].contrast,
-          fontSize: editItem[0].fontSize,
-          text: editItem[0].text,
-        }}
-        textLength="full"
-      />
     </div>
   );
 };
