9737aaf184e6d9a4a97674a43227ea6eb49382c5
Vilija Mažeikė <vmazeike@reiz.tech>
2023-06-10 16:03:34 +0300

    create new reader card

---
+++ src/components/cards/randomTextcard/RandomTextCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const RandomTextCard = (x: string[]) => {
+  const test = 'test';
+  return (
+    <div>
+      <div>{test}</div>
+      <div>{x}</div>
+    </div>
+  );
+};
---
+++ src/components/cards/randomTextcard/indext.tsx
@@ -0,0 +1,1 @@
+export { RandomTextCard } from './RandomTextCard';
