e0713346e6f7d953d67f7c2a7f51afbfd485d0b7
Vilija Mažeikė <vmazeike@reiz.tech>
2023-06-12 10:40:41 +0300

    use intro text as default value

--- src/utils/constants/defaults.tsx
+++ src/utils/constants/defaults.tsx
@@ -1,5 +1,8 @@
 import { generateFontSizes } from '../functions/fontArrGen';
 
+export const introText =
+  'In a digital world dominated by shallow forms of reading, Bionic reading aims to encourage a more in-depth reading and understanding of written content by facilitating the reading process by guiding the eyes through text with artificial fixation points.';
+
 export const defaultText = [
   'Bionic Reading is a new method facilitating the reading process by guiding the eyes through text with artificial fixation points. As a result, the reader is only focusing on the highlighted initial letters and lets the brain center complete the word. In a digital world dominated by shallow forms of reading, Bionic reading aims to encourage a more in-depth reading and understanding of written content.',
 ];
